8ca52dad031995e567e7265e87eed00f22f1cd33
Shashwat Dixit <shashwatmain@gmail.com>
2025-08-09 03:25:50 +0530

    Added a few packages

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, HTTPException
+import uvicorn
+
+items = []
+
+app = FastAPI(title="FastAPI", description="Trying out FastAPI")
+
+@app.get('/')
+def read_root():
+    return {'message': 'Welcome to FastAPI'}
+
+# Run with uvicorn
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
